ee69b7238b12574d555e7725f1f660400421886c
Justin Miranda <jcm62@columbia.edu>
2020-05-05 22:51:24 -0500

    OBGM-201 Fixed whitespace

--- grails-app/controllers/org/pih/warehouse/core/DocumentController.groovy
+++ grails-app/controllers/org/pih/warehouse/core/DocumentController.groovy
@@ -496,7 +496,7 @@ class DocumentController {
 /**
  * Command object
  */
-class DocumentCommand implements Validateable  {
+class DocumentCommand implements Validateable {
     String name
     String typeId
     String orderId
